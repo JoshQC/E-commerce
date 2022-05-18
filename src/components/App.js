3fd13b24976067999747eeef0f48cfeb3f7e664e
Joshua QC <joshua.quesada@uned.cr>
2022-05-17 20:59:04 -0600

    Agregando react router los componentes index y app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route, Link } from "react-router-dom";
 import ContenedorCategorias from "./Categorias/ContenedorCategorias/ContenedorCategorias";
+
+const Redireccionar = () => {
+  return <Link to={"/categorias"}>Ir a Categorias</Link>;
+};
 
 const App = () => {
   return (
-    <>
-      <ContenedorCategorias />
-    </>
+    <Routes>
+      <Route path="/" element={<Redireccionar />} />
+      <Route path="categorias" element={<ContenedorCategorias />} />
+    </Routes>
   );
 };
 
